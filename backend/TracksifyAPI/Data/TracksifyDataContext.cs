d0e8fcc25f1420b3ffa2f63aa68bdb0d4a261b8f
Emmanuel Adebanjo <adebanjoemmanuel01@gmail.com>
2024-01-02 23:58:10 +0100

    Initial File Configuration

---
+++ backend/TracksifyAPI/Data/TracksifyDataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TracksifyAPI.Data
+{
+    public class TracksifyDataContext : DbContext
+    {
+        public TracksifyDataContext(DbContextOptions<TracksifyDataContext> options) : base(options)
+        {
+            
+        }
+
+        //Create Database tables here using DbSet
+    }
+}
