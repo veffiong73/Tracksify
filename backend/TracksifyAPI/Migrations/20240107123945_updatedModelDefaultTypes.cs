e1e092df0044d18dc7db17e2ba99fb57312ca191
Emmanuel Adebanjo <adebanjoemmanuel01@gmail.com>
2024-01-08 08:25:38 +0100

    Update: changed the null strings to empty strings

---
+++ backend/TracksifyAPI/Migrations/20240107123945_updatedModelDefaultTypes.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TracksifyAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedModelDefaultTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
