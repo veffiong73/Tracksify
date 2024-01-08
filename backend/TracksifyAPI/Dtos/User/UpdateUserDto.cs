e5af41388ea342828218a16c4f05e828f308a4de
Emmanuel Adebanjo <adebanjoemmanuel01@gmail.com>
2024-01-08 03:44:50 +0100

    Created Dtos' for the user object

---
+++ backend/TracksifyAPI/Dtos/User/UpdateUserDto.cs
@@ -0,0 +1,15 @@
+﻿namespace TracksifyAPI.Dtos.User
+{
+    /**
+     * UpdateUserDto - this is a request Dto update a new User
+     */
+    public class UpdateUserDto
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+        public bool IsDeleted { get; set; } = false;
+    }
+}
