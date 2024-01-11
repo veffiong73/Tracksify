fc04e64e30bc933cc2b930956650bd37a62b14c7
Emmanuel Adebanjo <adebanjoemmanuel01@gmail.com>
2024-01-11 19:31:02 +0100

    Dto's for the project update Dto

---
+++ backend/TracksifyAPI/Dtos/ProjectUpdates/ProjectUpdateDto.cs
@@ -0,0 +1,18 @@
+﻿using TracksifyAPI.Models;
+
+namespace TracksifyAPI.Dtos.ProjectUpdates
+{
+    /**
+     *  ProjectUpdateDto - Response Dto to client on creation or updating of a projectUpdate
+     */
+    public class ProjectUpdateDto
+    {
+        public Guid ProjectUpdateId { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Today;
+        public DateTime CheckOut { get; set; }
+        public string WorkDone { get; set; } = string.Empty;
+        public Guid ProjectId { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
