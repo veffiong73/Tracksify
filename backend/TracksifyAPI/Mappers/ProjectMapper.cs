dab1ad0f74bab98dc9bbf3f896608a284966d65c
Duku Aghogho Mathida <mathildaduku@gmail.com>
2024-01-14 13:57:02 +0100

    Update ToProjectDto to include ProjectAssignees in the ProjectDto

--- backend/TracksifyAPI/Mappers/ProjectMapper.cs
+++ backend/TracksifyAPI/Mappers/ProjectMapper.cs
@@ -1,5 +1,9 @@
-﻿using TracksifyAPI.Dtos.Project;
+﻿using System.Collections.Generic;
+using TracksifyAPI.Data;
+using TracksifyAPI.Dtos.Project;
+using TracksifyAPI.Interfaces;
 using TracksifyAPI.Models;
+using TracksifyAPI.Repositories;
 
 namespace TracksifyAPI.Mappers
 {
@@ -12,6 +16,7 @@ namespace TracksifyAPI.Mappers
          * ToProjectDto - Converts a Project Object to a Dto
          * @projectModel: Project object to be converted
          */
+
         public static ProjectDto ToProjectDto(this Project projectModel)
         {
             return new ProjectDto
@@ -21,13 +26,16 @@ namespace TracksifyAPI.Mappers
                 StartDate = projectModel.StartDate,
                 DueDate = projectModel.DueDate,
                 ProjectDescription = projectModel.ProjectDescription,
-                ProjectStatus = projectModel.ProjectStatus
-            };
+                ProjectStatus = projectModel.ProjectStatus,
+                ProjectAssignees = projectModel.ProjectAssignees.Select(u => u.UserId).ToList(),
+                //ProjectAssignees = new List<User>(projectModel.ProjectAssignees), 
+                //ProjectAssignees = projectModel.ProjectAssignees.Select(u => new AddUserToProjectDto { UserId = u.UserId }).ToList()
+        };
         }
 
         /**
          * ToProjectFromCreateProjectDto - Converts a create project request Dto to a Project object
-         * userDto: The create project request Dto
+         * 
          */
         public static Project ToProjectFromCreateProjectDto(this CreateProjectDto projectDto) 
         {
@@ -36,7 +44,9 @@ namespace TracksifyAPI.Mappers
                  ProjectName = projectDto.ProjectName,
                  StartDate = projectDto.StartDate,
                  DueDate = projectDto.DueDate,
-                 ProjectDescription = projectDto.ProjectDescription 
+                 ProjectDescription = projectDto.ProjectDescription,
+                 // we should be able to get the user by user Id method in user repo
+                // ProjectAssignees = projectDto.ProjectAssignees.g,
             };
         }
     }
