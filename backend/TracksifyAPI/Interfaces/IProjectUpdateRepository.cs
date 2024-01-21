e324e72716817ed414fdd8a02aeb6d94b5162025
Emmanuel Adebanjo <adebanjoemmanuel01@gmail.com>
2024-01-21 12:52:58 +0100

    Update: fixed signature of some methods, added some new methods

--- backend/TracksifyAPI/Interfaces/IProjectUpdateRepository.cs
+++ backend/TracksifyAPI/Interfaces/IProjectUpdateRepository.cs
@@ -9,8 +9,9 @@ namespace TracksifyAPI.Interfaces
         Task<ProjectUpdate?> GetProjectUpdateByIdAsync(Guid projectUpdateId);
         Task<List<ProjectUpdate>> GetProjectUpdateByProjectIdAsync(Guid projectId);
         Task<List<ProjectUpdate>> GetProjectUpdateByUserIdAsync(Guid userId);
+        Task<List<ProjectUpdate>> GetUserProjectUpdateForSingleProjectByProjectIdAsync(Guid userId, Guid projectId);
         Task<ProjectUpdate> CreateProjectUpdateAsync(ProjectUpdate projectUpdateToCreate);
-        Task<bool> ProjectUpdateExistsForThatDay(Guid userId, DateTime dateCreated);
+        Task<bool> ProjectUpdateExistsForThatDay(Guid userId, Guid projectId, DateTime dateCreated);
         Task<bool> ProjectUpdateExistsAsync(Guid projectUpdateId);
 
     }
